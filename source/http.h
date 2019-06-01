ea0973f8aaaf0e0bb46a2d1654ee5d1718e6ce95
jborza <jborza@gmail.com>
2019-06-01 12:29:32 +0200

    removed text input from the html page, as it was unused

---
+++ source/http.h
@@ -0,0 +1,14 @@
+const static char http_200[] = "HTTP/1.1 200 OK\r\n";
+const static char http_201[] = "HTTP/1.1 201 Created\r\n";
+
+const static char indexdata[] = "<html> <head><body> \
+	<form method=\"POST\" action=\".\" enctype=\"multipart/form-data\">\
+			file: <input type=\"file\" name=\"file\">\
+			<input type=\"submit\">\
+		</form> \           
+	</body> \
+	</html>";
+
+const static char http_html_hdr[] = "Content-type: text/html\r\n\r\n";
+const static char http_get_index[] = "GET / HTTP/1.1\r\n";
+const static char http_post_index[] = "POST / HTTP/1.1\r\n";
--- source/uploader.c
+++ source/uploader.c
@@ -16,6 +16,8 @@
 
 #include <3ds.h>
 
+#include "http.h"
+
 #define DEBUG
 
 #ifdef DEBUG
@@ -32,21 +34,7 @@ s32 sock = -1, csock = -1;
 
 __attribute__((format(printf, 1, 2))) void failExit(const char *fmt, ...);
 
-const static char http_200[] = "HTTP/1.1 200 OK\r\n";
-const static char http_201[] = "HTTP/1.1 201 Created\r\n";
 
-const static char indexdata[] = "<html> <head><body> \
-	<form method=\"POST\" action=\".\" enctype=\"multipart/form-data\">\
-			file name: <input name=\"filename\"/><br/>\
-			file: <input type=\"file\" name=\"file\">\
-			<input type=\"submit\">\
-		</form> \           
-	</body> \
-	</html>";
-
-const static char http_html_hdr[] = "Content-type: text/html\r\n\r\n";
-const static char http_get_index[] = "GET / HTTP/1.1\r\n";
-const static char http_post_index[] = "POST / HTTP/1.1\r\n";
 
 const int DEFAULT_READ_SIZE = 1024;
 
